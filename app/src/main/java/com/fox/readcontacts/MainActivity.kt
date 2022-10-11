8eef71a882536073afdc722cb605b133449fbd4a
Foxxx48 <Foxxdim@mail.ru>
2022-10-11 14:50:12 +0300

    Add contentResolver for read contacts

--- app/src/main/java/com/fox/readcontacts/MainActivity.kt
+++ app/src/main/java/com/fox/readcontacts/MainActivity.kt
@@ -2,11 +2,11 @@ package com.fox.readcontacts
 
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.util.Log
-import androidx.annotation.RequiresApi
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.fox.readcontacts.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
@@ -16,8 +16,6 @@ class MainActivity : AppCompatActivity() {
     private val binding get() = _binding ?: throw RuntimeException("ActivityMainBinding = null")
 
 
-
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
@@ -31,44 +29,68 @@ class MainActivity : AppCompatActivity() {
         if (permissionGranted) {
             requestContacts()
         } else {
-            Log.d("MainActivity", "Permission denied")
+            requestPermission()
         }
-
-
-
-
-
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_CONTACTS),
+            READ_CONTACTS_RC
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_CONTACTS_RC && grantResults.isNotEmpty()) {
+            val permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+            if (permissionGranted) {
+                requestContacts()
+            } else {
+                Log.d("MainActivity", "Permission denied")
+            }
+
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
     private fun requestContacts() {
         thread {
-            val cursor = contentResolver.query(
-                ContactsContract.Contacts.CONTENT_FILTER_URI,
-                null,
-                null,
-                null
-            )
+            val cursor =
+                contentResolver.query(
+                    ContactsContract.Contacts.CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    null
+                )
+
             while (cursor?.moveToNext() == true) {
                 val id = cursor.getInt(cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID))
                 val name =
                     cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME))
 
                 val contact = Contacts(
-                    id = id,
-                    name = name
+                    id, name
                 )
-
                 Log.d("MainActivity", "$contact")
-
+                println("$contact")
             }
+            println("Cursor close()")
             cursor?.close()
-
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        private const val READ_CONTACTS_RC = 100
     }
 }
